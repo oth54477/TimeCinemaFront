1790f40ff833985e9d2b8f03c202544b7b2136fd
oth54477 <oth5447@naver.com>
2022-11-16 01:39:42 +0900

    Day01

---
+++ cinema/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <button @click="load">load</button>
+    <LoadingPage class="loading" v-if="!isLoaded"/>
+    <MenuBar />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import LoadingPage from '@/components/LoadingPage'
+import MenuBar from '@/components/MenuBar'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isLoaded: false
+    }
+  },
+  components: {
+    LoadingPage,
+    MenuBar,
+  },
+  methods: {
+    load() {
+      this.isLoaded = !this.isLoaded
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: gray;
+
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* display: grid; */
+  /* grid-template-areas: ""; */
+  position: relative;
+}
+
+.loading {
+  width: 300px;
+  position: absolute;
+  top: 500px;
+  left: 1200px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ cinema/src/components/LoadingPage.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <div class="clock"></div>
+      <p>Clock</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LoadingPage',
+  methods: {
+    
+  }
+
+}
+</script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Lato:300);
+
+
+.container{
+  text-align: center;
+  /* background-color: gray; */
+  overflow: hidden;
+}
+
+/* .box:nth-child(2n-1){
+  background-color: rgba(0,0,0,0.05);
+} */
+
+.box{
+  display: inline-block;
+  height: 200px;
+  width: 33.3%;
+  float:left;
+  position: relative;
+  /*margin:0 -4px -5px -2px;*/
+  transition: all .2s ease;
+}
+
+
+
+.box p{
+  color: #777;
+  font-family:  Lato,"Helvetica Neue" ;
+  font-weight: 300;
+  position: absolute;
+  font-size: 20px;
+  width: 100%;
+  height: 25px;
+  text-align: center;
+  bottom: 0px;
+  margin: 0;
+  top:160px;
+  background-color: #fff;
+  opacity: 0;
+  text-transform: uppercase;
+  transition: all .2s ease;
+}
+
+.box:hover p{
+  bottom:0px;
+  top:175px;
+  opacity: 1;
+  transition: all .2s ease;
+  z-index: 2;
+}
+
+
+
+/* MEDIA QUERIES */
+@media (max-width: 700px){
+  .box{
+    width: 50%;
+  }
+
+  .box:nth-child(2n-1){
+    background-color: inherit;
+  }
+
+  .box:nth-child(4n),.box:nth-child(4n-3) {
+    background-color: rgba(0,0,0,0.05);
+  }
+
+}
+
+@media (max-width: 420px){
+  .box{
+    width: 100%;
+  }
+
+  .box:nth-child(4n),.box:nth-child(4n-3){
+    background-color: inherit;
+  }
+
+  .box:nth-child(2n-1){
+    background-color:rgba(0,0,0,0.05);
+  }
+
+}
+
+
+/* -------------- Clock -------------- */
+
+.clock{
+  border-radius: 60px;
+  border: 3px solid #fff;
+  height: 80px;
+  width: 80px;
+  position: relative;
+
+  top: 28%;
+  top: -webkit-calc(50% - 43px);
+  top: calc(50% - 43px);
+  left: 35%;
+  left: -webkit-calc(50% - 43px);
+  left: calc(50% - 43px);
+}
+.clock:after{
+  content: "";
+  position: absolute;
+  background-color: #fff;
+  top:2px;
+  left: 48%;
+  height: 38px;
+  width: 4px;
+  border-radius: 5px;
+  -webkit-transform-origin: 50% 97%;
+      transform-origin: 50% 97%;
+  -webkit-animation: grdAiguille 2s linear infinite;
+      animation: grdAiguille 2s linear infinite;
+}
+
+@-webkit-keyframes grdAiguille{
+    0%{-webkit-transform:rotate(0deg);}
+    100%{-webkit-transform:rotate(360deg);}
+}
+
+@keyframes grdAiguille{
+    0%{transform:rotate(0deg);}
+    100%{transform:rotate(360deg);}
+}
+
+.clock:before{
+  content: "";
+  position: absolute;
+  background-color: #fff;
+  top:6px;
+  left: 48%;
+  height: 35px;
+  width: 4px;
+  border-radius: 5px;
+  -webkit-transform-origin: 50% 94%;
+      transform-origin: 50% 94%;
+  -webkit-animation: ptAiguille 12s linear infinite;
+      animation: ptAiguille 12s linear infinite;
+}
+
+@-webkit-keyframes ptAiguille{
+    0%{-webkit-transform:rotate(0deg);}
+    100%{-webkit-transform:rotate(360deg);}
+}
+
+@keyframes ptAiguille{
+    0%{transform:rotate(0deg);}
+    100%{transform:rotate(360deg);}
+}
+</style>
